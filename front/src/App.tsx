caf7378f8ab6b32b0436d7c52fdb0021a081a9d9
Samantha Le Huec <s.lhuec@gmail.com>
2023-08-23 15:30:20 +0200

    test css

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './duck.png';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          ~ Magical ducky pong ~
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Connexion with 42
         </a>
       </header>
     </div>
