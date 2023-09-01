e6b9bbfbab542fba90e3122835ab37f7a7539e2c
Samantha Le Huec <s.lhuec@gmail.com>
2023-09-01 09:35:56 +0200

    save state

---
+++ front/src/Auth.tsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState, useContext}  from "react";
+
+//herE declaRER LES VAR 
+// const blaBla = kdjhge
+
+// here : declarer function pour reperer l'event (??)
+// here : declarer function pour trouver code
+
+ 
+
+
+// here: declarer const function  avec useEffect pour fetch
+
+ 
+
+// here: declarer const function  avec useSte pour stocker
+
+
+//TODO traduire en typsecript
