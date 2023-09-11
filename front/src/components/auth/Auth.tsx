d3adc2e10caad62d4e9d7a629e3a4039fcde947c
Samantha Le Huec <s.lhuec@gmail.com>
2023-09-11 12:16:45 +0200

    little fix on fetch front to back

--- front/src/components/auth/Auth.tsx
+++ front/src/components/auth/Auth.tsx
@@ -4,34 +4,34 @@ import FormData from 'form-data';
 
 function getCode() {
 	let url_str = window.location.search;
-	console.log(url_str);
+	// console.log(url_str);
 	let strToSearch = new URLSearchParams(url_str);
 	let code_param = strToSearch.get("code");
 	return code_param;
 }
 
 let codeP = getCode();
-console.log(codeP);
+// console.log(codeP);
 
+if (codeP != null) {
 
-if (codeP != null )
-{
 	//FIXME fetch sur le service de Nathan
-
-//j'appelle une fonction du back pour 'envoyer à Nat
-	let form = new FormData();
+	const form = new FormData();
 	form.append("code", codeP);
 	fetch('http://localhost:3000/auth/login', {
 		method: "POST",
 		mode: "no-cors",
-		body: JSON.stringify(codeP)
+		body: form,
 	}).then((response) => {
-			if (!response.ok) {
-				throw new Error(JSON.stringify(response.statusText));
-			}
-		}).catch(err => {
-			console.error(`Fetch to backend for authentication failed: ${err}`);
-		});
+		if (!response.ok) {
+			throw new Error(JSON.stringify(response.status));
+			// console.log(response)
+		}
+	}).catch(err => {
+		// console.error(`Fetch to backend for authentication failed: ${err}`);
+		console.log("err in catch: ")
+		console.log(err)
+	});
 }
 
 
