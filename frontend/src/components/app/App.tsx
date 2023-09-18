23f581fbfb6b0f036f73e02d83d7453418efc524
Samantha Le Huec <s.lhuec@gmail.com>
2023-09-18 16:14:16 +0200

    refacto appli: C P T

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,27 +1,15 @@
-import React from 'react';
-import logo from '../../assets/duck.png';
-import '../auth/Auth' 
-import './App.css';
-
-const clientid = process.env.REACT_APP_CLIENT_ID;
+import React from "react";
+import { useRoutes, Routes, Route } from "react-router-dom";
+import Login from "../auth/Login";
+import Home from "../home/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ~ Magical ducky pong ~
-        </p>
-        <a
-          className="App-link"
-          href={`https://api.intra.42.fr/oauth/authorize?client_id=${clientid}&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2F&response_type=code`}
-        >
-          Connexion with 42
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Routes>
+			<Route path={"/"} element={<Login />} />
+			<Route path={"/home"} element={<Home />} />
+		</Routes>
+	)
 }
 
-export default App;
+export default App
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './components/app/App';
-import reportWebVitals from './reportWebVitals';
 import Home from './components/home/Home';
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
-
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
