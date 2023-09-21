f8d9a1b67a57372443edb442836095a71cfd6649
Samantha Le Huec <s.lhuec@gmail.com>
2023-09-21 16:06:30 +0200

    fix

--- frontend/src/components/auth/Auth.tsx
+++ frontend/src/components/auth/Auth.tsx
@@ -34,6 +34,7 @@ const authProcess = async () => {
 	if (datas.status === 302) {
 		const newUrl = datas.url;
 		window.location.href = newUrl;
+		console.log("bearer token: ", datas.access_token);
 	}
 
 	// FIXME :
