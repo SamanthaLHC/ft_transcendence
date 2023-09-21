d1e2e041bdf8258bc0cf7725867fb1307c3b5b5b
Basile REGNEAU <bregneau@student.42.fr>
2023-09-21 19:27:18 +0200

    WIP: get channels from db 	modified:   src/chat/chat.controller.ts 	modified:   src/chat/chat.service.ts

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Channels, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+	getAllChannels() {
+		const channels = prisma.channels.findMany({
+			select: {
+				name: true,
+			},
+		});
+		return channels;
+	}
+
+	getChannelByName(channelName: string) {
+		const channel = prisma.channels.findFirst({
+			where: {
+				name: channelName,
+			}
+		});
+		return channel;
+
+	}
+}
