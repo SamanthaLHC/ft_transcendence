f3977a57cc699e730af9cfce0197ea67b9b4a40e
Basile REGNEAU <bregneau@student.42.fr>
2023-09-21 19:27:18 +0200

    WIP: get channels from db 	modified:   src/chat/chat.controller.ts 	modified:   src/chat/chat.service.ts

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { Public } from 'src/auth/decorators/public.decorator';
 
+
+
+
+@Public()
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+	constructor(private readonly chatService: ChatService) {}
+
+	@Get()
+	getAllChannels() {
+		return this.chatService.getAllChannels();
+	}
+
+	@Get(':channelName')
+	getChannelByName(@Param('channelName') channelName: string) {
+		return this.chatService.getChannelByName(channelName);
+	}
+
+}
