dd6385af1bad0d6005ba7252e9e754bb86b8c310
Samantha Le Huec <s.lhuec@gmail.com>
2023-09-21 13:02:51 +0200

    kitsh button but not responsive

--- frontend/src/components/home/ButtonPlay.tsx
+++ frontend/src/components/home/ButtonPlay.tsx
@@ -1,54 +1,166 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
 import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import coin from '../../assets/Rubber_Duck_in_Parramatta_Park.jpg'
 
-// Augment the palette to include an ochre color
-declare module '@mui/material/styles' {
-	interface Palette {
-		ochre: Palette['primary'];
-	}
+const images = [
+	{
+		url: coin,
+		title: 'Play this magical game, right now!',
+		width: '100%',
+	},
+]
 
-	interface PaletteOptions {
-		ochre?: PaletteOptions['primary'];
-	}
-}
 
-// Update the Button's color options to include an ochre option
-declare module '@mui/material/Button' {
-	interface ButtonPropsColorOverrides {
-		ochre: true;
-	}
-}
-
-const theme = createTheme({
-	palette: {
-		ochre: {
-			main: '#ffc107'
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+	position: 'relative',
+	height: 450,
+	[theme.breakpoints.down('sm')]: {
+		width: '100% !important', // Overrides inline-style
+		height: 100,
+	},
+	'&:hover, &.Mui-focusVisible': {
+		zIndex: 1,
+		'& .MuiImageBackdrop-root': {
+			opacity: 0.15,
+		},
+		'& .MuiImageMarked-root': {
+			opacity: 0,
+		},
+		'& .MuiTypography-root': {
+			border: '4px solid currentColor',
 		},
 	},
+}));
 
-	typography: {
-		fontFamily: "Azonix"
-	}
+const ImageSrc = styled('span')({
+	position: 'absolute',
+	left: 0,
+	right: 0,
+	top: 0,
+	bottom: 0,
+	backgroundSize: 'cover',
+	backgroundPosition: 'center 100%',
 });
 
+const Image = styled('span')(({ theme }) => ({
+	position: 'absolute',
+	left: 20,
+	right: 20,
+	top: 0,
+	bottom: 0,
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	color: theme.palette.common.white,
+}));
 
-function ButtonPlay() {
+const ImageBackdrop = styled('span')(({ theme }) => ({
+	position: 'absolute',
+	left: 0,
+	right: 0,
+	top: 0,
+	bottom: 0,
+	backgroundColor: theme.palette.common.black,
+	opacity: 0.4,
+	transition: theme.transitions.create('opacity'),
+}));
+
+const ImageMarked = styled('span')(({ theme }) => ({
+	height: 3,
+	width: 18,
+	backgroundColor: theme.palette.common.white,
+	position: 'absolute',
+	bottom: -2,
+	left: 'calc(50% - 9px)',
+	transition: theme.transitions.create('opacity'),
+}));
+
+export default function ButtonPlay() {
 	return (
-
-
-		<ThemeProvider theme={theme}>
-			<Button
-				sx={{ width: 200, height: 90 }}
-				variant="contained"
-				size='large'
-				color="ochre"
-			>
-				Play this magical game, right now!
-			</Button>
-		</ThemeProvider >
+		<Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 500, width: '100%' }}>
+			{images.map((image) => (
+				<ImageButton
+					focusRipple
+					key={image.title}
+					style={{
+						width: image.width,
+					}}
+				>
+					<ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+					<ImageBackdrop className="MuiImageBackdrop-root" />
+					<Image>
+						<Typography
+							component="span"
+							variant="subtitle1"
+							color="inherit"
+							sx={{
+								position: 'relative',
+								p: 4,
+								pt: 2,
+								pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+							}}
+						>
+							{image.title}
+							<ImageMarked className="MuiImageMarked-root" />
+						</Typography>
+					</Image>
+				</ImageButton>
+			))}
+		</Box>
 	);
 }
 
-export default ButtonPlay
+// // Augment the palette to include an ochre color
+// declare module '@mui/material/styles' {
+// 	interface Palette {
+// 		ochre: Palette['primary'];
+// 	}
+
+// 	interface PaletteOptions {
+// 		ochre?: PaletteOptions['primary'];
+// 	}
+// }
+
+// // Update the Button's color options to include an ochre option
+// declare module '@mui/material/Button' {
+// 	interface ButtonPropsColorOverrides {
+// 		ochre: true;
+// 	}
+// }
+
+// const theme = createTheme({
+// 	palette: {
+// 		ochre: {
+// 			main: '#ffc107'
+// 		},
+// 	},
+
+// 	typography: {
+// 		fontFamily: "Azonix"
+// 	}
+// });
+
+
+// function ButtonPlay() {
+// 	return (
+
+
+// 		<ThemeProvider theme={theme}>
+// 			<Button
+// 				sx={{ width: 200, height: 90 }}
+// 				variant="contained"
+// 				size='large'
+// 				color="ochre"
+// 			>
+// 				Play this magical game, right now!
+// 			</Button>
+// 		</ThemeProvider >
+// 	);
+// }
+
+// export default ButtonPlay
 
--- frontend/src/components/home/friends/SearchBar.tsx
+++ frontend/src/components/home/friends/SearchBar.tsx
@@ -4,44 +4,42 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
 function Search() {
-    const [searchText, setSearchText] = useState('');
+	const [searchText, setSearchText] = useState('');
 
-    // const handleSearchChange = (e) => {
-    //     setSearchText(e.target.value);
+	// const handleSearchChange = (e) => {
+	//     setSearchText(e.target.value);
 };
 
 const handleSearch = () => {
-    // Implement your search logic here
-    // console.log(`Searching for: ${searchText}`);
+	// Implement your search logic here
+	// console.log(`Searching for: ${searchText}`);
 };
 
 
 export default function SearchBar() {
-
-    return (
-        <div >
-            <TextField
-                label="Search"
-                variant="outlined"
-                fullWidth
-                // value={searchText}
-                // onChange={handleSearchChange}
-                style={{ backgroundColor: '#42464f' }}
-                InputProps={{
-                    style: { color: '#ffc107' }, // Set the background color to #ffc107 (yellow)
-                    endAdornment: (
-                        <IconButton 
-                        style={{ color: '#ffc107' }}
-                        onClick={handleSearch}
-                        >
-                            <SearchIcon />
-                        </IconButton>
-                    ),
-                }}
-            />
-        </div>
-    );
-
+	return (
+		<div>
+			<TextField
+				label="Search"
+				variant="outlined"
+				fullWidth
+				// value={searchText}
+				// onChange={handleSearchChange}
+				style={{ backgroundColor: '#42464f' }}
+				InputProps={{
+					style: { color: '#ffc107' }, // Set la couleur du texte à #ffc107 (jaune)
+					endAdornment: (
+						<IconButton
+							style={{ color: '#ffc107' }}
+							onClick={handleSearch}
+						>
+							<SearchIcon />
+						</IconButton>
+					),
+				}}
+			/>
+		</div>
+	);
 }
 
 
