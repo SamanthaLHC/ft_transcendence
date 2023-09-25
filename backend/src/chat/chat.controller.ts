221c9221f5f42f0c6c4ba16e25fc433487898693
Basile REGNEAU <bregneau@student.42.fr>
2023-09-25 11:05:30 +0200

    WIP channels 	modified:   backend/src/chat/chat.controller.ts 	modified:   backend/src/chat/chat.service.ts

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -1,23 +1,26 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { Public } from 'src/auth/decorators/public.decorator';
-
-
-
 
 @Public()
 @Controller('chat')
 export class ChatController {
 	constructor(private readonly chatService: ChatService) {}
 
+		
 	@Get()
-	getAllChannels() {
-		return this.chatService.getAllChannels();
+	async getAllChannels() {
+		return await this.chatService.getAllChannels();
 	}
 
 	@Get(':channelName')
-	getChannelByName(@Param('channelName') channelName: string) {
-		return this.chatService.getChannelByName(channelName);
+	async getChannelByName(@Param('channelName') channelName: string) {
+		return await this.chatService.getChannelByName(channelName);
+	}
+
+	@Post()
+	createChannel(@Body() newChannel) {
+		return this.chatService.createChannel(newChannel.channelName, newChannel.privacy, newChannel.ownerId);
 	}
 
 }
--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { Channels, PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient()
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Privacy } from '@prisma/client';
+import { PrismaService, } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ChatService {
-	getAllChannels() {
-		const channels = prisma.channels.findMany({
+	constructor(private readonly prisma: PrismaService)	{}
+
+	
+	async getAllChannels() {
+		const channels = await this.prisma.channels.findMany({
 			select: {
 				name: true,
 			},
 		});
+		// if (!channels)
+		// 	throw new NotFoundException('No channels found');
 		return channels;
 	}
 
-	getChannelByName(channelName: string) {
-		const channel = prisma.channels.findFirst({
+	async getChannelByName(channelName: string){
+		const channel = await this.prisma.channels.findUnique({
 			where: {
 				name: channelName,
 			}
 		});
+		if (!channel)
+			throw new NotFoundException("Channel not found");
 		return channel;
+	}
 
+	async createChannel(channelName: string, privacy: Privacy, ownerId: number) {
+		const channel = await this.prisma.channels.create({
+			data: {
+				name: channelName,
+				privacy: privacy,
+				ownerId: ownerId,
+			}
+		});
+		return channel;
 	}
 }
