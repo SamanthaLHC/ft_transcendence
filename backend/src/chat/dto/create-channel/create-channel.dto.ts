6eb123846e49230a9e6d4430986a0471d9eec489
Basile REGNEAU <bregneau@student.42.fr>
2023-09-27 14:57:44 +0200

    add createChannel method and createChannelDto 	modified:   backend/src/chat/chat.controller.ts 	modified:   backend/src/chat/chat.service.ts 	new file:   backend/src/chat/dto/create-channel/create-channel.dto.ts

---
+++ backend/src/chat/dto/create-channel/create-channel.dto.ts
@@ -0,0 +1,17 @@
+import { Privacy } from "@prisma/client";
+import { IsEnum, IsNotEmpty, IsNumber, IsPositive, IsString, Matches, Max, MaxLength } from "class-validator";
+
+export class CreateChannelDto {
+	@IsNotEmpty()
+	@MaxLength(100)
+	@IsString()
+	@Matches(/^[A-Za-z0-9_-]*$/)
+	name: string;
+
+	@IsEnum(Privacy)
+	privacy: Privacy;
+
+	@IsNumber()
+	@IsPositive()
+	ownerId: number;
+}
