e8b36e4924554f3bafbbe21cae3ff9623adb668e
Samantha Le Huec <s.lhuec@gmail.com>
2023-10-16 17:44:24 +0200

    match history done

--- frontend/src/components/profil/MatchHistory.tsx
+++ frontend/src/components/profil/MatchHistory.tsx
@@ -3,29 +3,24 @@ import { useCookies } from "react-cookie";
 import { useUser } from "../Context";
 
 interface MatchData {
-    id: number;
-    login: string;
-    name: string;
+    gameId: number;
+    gagnantId: number;
+    perdantId: number;
+    scoreGagnant: number;
+    scorePerdant: number;
+    at: string;
+    winnerName: string;
+    loserName: string;
 }
 
-//FIXME : mauvais MAJ de userData ? --> probleme de Context
-//FIXME : useEffect ? --> semble être appelé deux fois
-
 const MatchHistory: React.FC = () => {
-
     const [cookies] = useCookies(['access_token']);
-    const [matchData, setRankingData] = useState<MatchData[]>([]);
+    const [matchData, setMatchData] = useState<MatchData[]>([]);
     const { userData } = useUser();
-    console.log("in userDATA: ", userData);
     const idUser = userData.id;
-    console.log("user id is: ", userData.id);
 
     useEffect(() => {
-
         const getMatch = async () => {
-
-            console.log("idUser: ", idUser);
-            console.log(`http://localhost:3000/users/gameHistorique/${idUser}`);
             try {
                 const req = new Request(`http://localhost:3000/users/gameHistorique/${idUser}`, {
                     method: "GET",
@@ -35,47 +30,93 @@ const MatchHistory: React.FC = () => {
                 });
 
                 const response = await fetch(req);
+
                 if (response.ok) {
-                    console.log(response);
                     const data = await response.json();
+                    setMatchData(data);
                 } else {
-                    console.log("BAD BAD");
-                    // setAvatarUploadError("Avatar upload failed. Please try again.");
+                    console.log("API request failed");
                 }
             } catch (error) {
-                console.error(error);
-                // setAvatarUploadError("An error occurred while uploading your avatar.");
+                console.error("An error occurred while fetching match data:", error);
             }
         };
         getMatch();
     }, [cookies.access_token, idUser]);
 
+    // Function to get user name by ID
+    const getUserNameById = async (userId: number) => {
+        try {
+            const req = new Request(`http://localhost:3000/users/id/${userId}`, {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${cookies.access_token}`,
+                },
+            });
+
+            const response = await fetch(req);
+
+            if (response.ok) {
+                const user = await response.json();
+                return user.name;
+            } else {
+                console.log("request failed");
+                return "Unknown";
+            }
+        } catch (error) {
+            console.error("An error occurred while fetching user data:", error);
+            return "Unknown";
+        }
+    };
+
+    useEffect(() => {
+        const fetchUserNames = async () => {
+            const promises = matchData.map((item) => {
+                return Promise.all([getUserNameById(item.gagnantId), getUserNameById(item.perdantId)]);
+            });
+
+            Promise.all(promises).then((userNames) => {
+                const updatedMatchData = matchData.map((item, index) => ({
+                    ...item,
+                    winnerName: userNames[index][0],
+                    loserName: userNames[index][1],
+                }));
+                setMatchData(updatedMatchData);
+            });
+        };
+
+        if (matchData.length > 0) {
+            fetchUserNames();
+        }
+    }, [matchData]);
+
     return (
         <React.Fragment>
-            <h2 className="typo-friends yellow typo-pos">Match History </h2>
-            <div>
-                <table className="styled-table">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>your matchs</th>
+            <h2 className="typo-friends yellow typo-pos">Match History</h2>
+            <table className="styled-table">
+                <thead>
+                    <tr>
+                        <th>Match </th>
+                        <th>Winner</th>
+                        <th>Loser</th>
+                        <th>Winner Score</th>
+                        <th>Loser Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {matchData.map((item, index) => (
+                        <tr className="active-row" key={index}>
+                            <td>{item.gameId}</td>
+                            <td>{item.winnerName}</td>
+                            <td>{item.loserName}</td>
+                            <td>{item.scoreGagnant}</td>
+                            <td>{item.scorePerdant}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="active-row">
-                            <td>1</td>
-                            <td></td>
-                        </tr>
-                        <tr className="active-row">
-                            <td>2</td>
-                            <td></td>
-
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                    ))}
+                </tbody>
+            </table>
         </React.Fragment>
-    )
+    );
 }
 
 export default MatchHistory;
