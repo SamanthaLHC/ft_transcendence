dec6fa75c3c44c247058893a6c579691536b6438
Margaux Riant <mriant@student.42.fr>
2023-10-16 15:54:49 +0200

    Change changeRoom function to be called on a emit from the front
     Changes to be committed:
    	modified:   backend/src/chat/chat.gateway.ts
    	modified:   backend/src/chat/chat.service.ts
    	new file:   backend/src/chat/dto/chat_socket.dto.ts

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,23 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer, MessageBody, ConnectedSocket } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { ChatSocketDto } from './dto/chat_socket.dto';
 
 @WebSocketGateway({ cors: { origin:['http://localhost:8000'] }})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-	room: string
-	socket: Socket;
+	sockets: [ChatSocketDto];
 
 	@WebSocketServer()
 	server: Server
 
 	handleConnection(socket: Socket) {
-		this.socket = socket
-		console.log("coucou chat")
+		let newSocket = new ChatSocketDto() 
+		newSocket.socket = socket
+		newSocket.room = ""
+		newSocket.user = ""
+		if (this.sockets === undefined) {
+			this.sockets = [newSocket]
+		} else {
+			this.sockets.push(newSocket)
+		}
+		let findSocket = this.sockets.find(sockets => sockets.socket === socket)
+		let pos = this.sockets.indexOf(findSocket);
+		console.log("coucou chat " + pos)
 		socket.emit('lalalalala', "coucou")
 	}
 	
-	handleDisconnect(client: any) {
-	
+	handleDisconnect(@ConnectedSocket() client: Socket) {
+		let findSocket = this.sockets.find(sockets => sockets.socket === client)
+		let pos = this.sockets.indexOf(findSocket);
+		let removedItem = this.sockets.splice(pos, 1);
+		console.log("User " + removedItem["user"] + " was disconnected from " + removedItem["socket"])
 	}
 
 	@SubscribeMessage('message')
@@ -27,22 +40,23 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	this.server.emit('lalalalala', data); // Broadcast the message to all connected clients
 	}
 
-	changeRoom(new_room: string) {
-		if (this.room) {
-			this.socket.leave(this.room)
-			console.log ("Change from ", this.room, " to ", new_room)
+	@SubscribeMessage('change_room')
+	changeRoom(@MessageBody() new_room: string, @ConnectedSocket() client: Socket) {
+		let findSocket = this.sockets.find(sockets => sockets.socket === client)
+		if (findSocket.room !== "") {
+			findSocket.socket.leave(findSocket.room)
+			console.log ("Change from ", findSocket.room, " to ", new_room)
 		}
-		this.socket.join(new_room)
+		findSocket.socket.join(new_room)
 		console.log("Joining " + new_room)
-		this.room = new_room
+		console.log("Before change " + findSocket.room)
+		findSocket.room = new_room
+		let findSocket2 = this.sockets.find(sockets => sockets.socket === client)
+		console.log("After change " + findSocket2.room)
 	}
 	
-	getRoom() {
-		return this.room;
-	}
-	
-	broadCast(event: string) {
-		console.log("Emiting to " + this.room)
-		this.server.to(this.room).emit("update", this.room + event)
-	}
+	// broadCast(event: string) {
+	// 	console.log("Emiting to " + this.room)
+	// 	this.server.to(this.room).emit("update", this.room + event)
+	// }
 }
