b2f6c0b64695f7f4fae0acb15677fb2f759fea43
Basile REGNEAU <bregneau@student.42.fr>
2023-10-18 16:26:47 +0200

    modified popup: now we have a radio box to choose between public and protected channel 	modified:   frontend/src/components/chat/CreateChannelForm.tsx

--- frontend/src/components/chat/CreateChannelForm.tsx
+++ frontend/src/components/chat/CreateChannelForm.tsx
@@ -7,55 +7,69 @@ interface CreateChannelFormProps {
 
 const CreateChannelForm: React.FC<CreateChannelFormProps> = ({ isOpen, onSubmit }) => {
 	const [name, setName] = useState('');
-	const [privacy, setPrivacy] = useState('PUBLIC');
+	const [isProtected, setIsProtected] = useState(false);
 	const [password, setPassword] = useState('');
 
 	if (!isOpen) {
 		return null;
 	}
 
+	const handlePrivacyChange = () => {
+		setIsProtected(!isProtected);
+	};
+
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		onSubmit(name, privacy, privacy === 'PROTECTED' ? password : undefined);
+		const privacy = isProtected ? 'PASSWORD_PROTECTED' : 'PUBLIC';
+		onSubmit(name, privacy, isProtected ? password : undefined);
 	};
 
 	const isCreateButtonDisabled = name.trim() === '';
 
 	return (
 		<div className="popup">
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="name">Name:</label>
-				<input
-					type="text"
-					id="name"
-					value={name}
-					onChange={(event) => setName(event.target.value)}
-				/>
-				<br />
+			<form onSubmit={handleSubmit} style={{ textAlign: 'left' }}>
+				<div style={{ marginBottom: '10px' }}>
+					<label htmlFor="name" style={{ marginRight: '10px', fontSize: '14px' }}>
+						Name:
+					</label>
+					<input
+						type="text"
+						id="name"
+						value={name}
+						onChange={(event) => setName(event.target.value)}
+						style={{ width: '150px' }}
+					/>
+				</div>
 
-				<label htmlFor="privacy">Privacy:</label>
-				<select
-					id="privacy"
-					value={privacy}
-					onChange={(event) => setPrivacy(event.target.value)}
-				>
-					<option value="PUBLIC">PUBLIC</option>
-					<option value="PRIVATE">PRIVATE</option>
-					<option value="PROTECTED">PROTECTED</option>
-				</select>
-				<br />
+				<div style={{ marginBottom: '10px' }}>
+					<label style={{ fontSize: '14px' }}>
+						<input
+							type="checkbox"
+							checked={isProtected}
+							onChange={handlePrivacyChange}
+							style={{ marginRight: '5px' }}
+						/>
+						Protected
+					</label>
+				</div>
 
-				{privacy === 'PROTECTED' && (
-					<>
-						<label htmlFor="password">Password:</label>
+				{isProtected && (
+					<div style={{ marginBottom: '10px' }}>
+						<label
+							htmlFor="password"
+							style={{ marginRight: '10px', fontSize: '14px' }}
+						>
+							Password: 
+						</label>
 						<input
 							type="password"
 							id="password"
 							value={password}
 							onChange={(event) => setPassword(event.target.value)}
+							style={{ width: '150px' }}
 						/>
-						<br />
-					</>
+					</div>
 				)}
 
 				<button
