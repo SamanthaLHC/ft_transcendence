463a53edd36baa667813da21322a3d531b5401de
Basile REGNEAU <bregneau@student.42.fr>
2023-10-25 15:07:11 +0200

    Add : When a user creates a channel, he is automatically sent to it 	modified:   backend/src/chat/chat.gateway.ts 	modified:   backend/src/chat/chat.service.ts 	modified:   frontend/src/components/Context.tsx 	modified:   frontend/src/components/chat/WindowChat.tsx 	modified:   frontend/src/components/chat/channels/ChannelButton.tsx 	modified:   frontend/src/components/chat/channels/Channels.tsx

--- frontend/src/components/chat/channels/ChannelButton.tsx
+++ frontend/src/components/chat/channels/ChannelButton.tsx
@@ -4,42 +4,28 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 import { useChatSocket } from '../../Context';
-import { useCookies } from "react-cookie";
 
 interface Channel {
 	id: number;
 	name: string;
+	privacy: string;
 }
 
 interface ChannelButtonProps {
 	channel: Channel;
 }
 
-
 const ChannelButton: React.FC<ChannelButtonProps> = ({ channel }) => {
 	const socket = useChatSocket()
-	const [cookies] = useCookies(["access_token"]);
-	console.log(channel)
+
+	const changeChannel = (channel : Channel) => {
+		socket.socket.emit('change_room', channel.name);
+		socket.channel = channel
+	}
 
 	const handleChannelClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-		const name = channel.name
-		socket.socket.emit('change_room', name);
-		if (name)
-			socket.room = name
-		const req = new Request("http://localhost:3000/chat/channel/" + name, {
-			method: "GET",
-			headers: {
-				Authorization: `Bearer ${cookies.access_token}`,
-			},
-		})
-		fetch(req)
-			.then((response) => response.json())
-			.then((data) => {
-				console.log("data:", data);
-			})
-			.catch((error) => {
-				console.error("Error fetching channels:", error);
-			});
+		event.preventDefault();
+		changeChannel(channel)
 	}
 
 	return (
