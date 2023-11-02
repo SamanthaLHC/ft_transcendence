2504de157bf42f838b11fea12833d071bfe68ba9
Margaux Riant <mxriant@gmail.com>
2023-11-02 16:27:01 +0100

    Add: Material Ui in Messages buttons
     Changes to be committed:
    	modified:   frontend/package-lock.json
    	modified:   frontend/package.json
    	modified:   frontend/src/components/chat/MessageChat.tsx

--- frontend/src/components/chat/MessageChat.tsx
+++ frontend/src/components/chat/MessageChat.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react'
+import { Box, Button, ButtonGroup, createTheme, ThemeProvider } from '@mui/material'
+import React from 'react'
 import { useUser } from '../Context'
 
 interface Message {
@@ -11,33 +12,85 @@ interface MessageProps {
 	message: Message
 }
 
+declare module '@mui/material/styles' {
+	interface Palette {
+		ochre: Palette['primary'];
+	}
+
+	interface PaletteOptions {
+		ochre?: PaletteOptions['primary'];
+	}
+}
+
+// Update the Button's color options to include an ochre option
+declare module '@mui/material/Button' {
+	interface ButtonPropsColorOverrides {
+		ochre: true;
+	}
+}
+
+const theme = createTheme({
+	palette: {
+		ochre: {
+			main: '#ffc107'
+		},
+	},
+
+	typography: {
+		fontFamily: "azonix"
+	}
+});
+
 const MessageChat: React.FC<MessageProps> = ({ message }) => {
 
 	const { userData } = useUser();
-	console.log("mess type ",message.type)
+	console.log("mess type ", message.type)
 	if (message.type === "MESSAGE") {
 		return (
 			<span><b>{message.sender + ":"}</b><br></br>{message.msg}</span>
 		)
 	}
 	else {
-		if (message.sender === userData.name)
-		{
+		if (message.sender === userData.name) {
 			return (
 				<span><b>{message.sender + ":"}</b><br></br>{message.msg}</span>
 			)
 		}
-		else
-		{
-		return (
-			<span><b>{message.sender + ":"}</b><br></br>{message.msg}
-				<br></br>
+		else {
+			return (
+				<span><b>{message.sender + ":"}</b><br></br>{message.msg}
+					<br></br>
 					<div className='btn-pos'>
-						<button className="btn-size" >Accept</button>
-						<button className="btn-size" >Refuse</button>
+						<Box
+							sx={{
+								display: 'flex',
+								flexDirection: 'row',
+								alignItems: 'flex-start',
+							}}
+						>
+							<ThemeProvider theme={theme}>
+
+								<ButtonGroup
+									variant="outlined"
+									aria-label="outlined button group">
+									<Button
+										size='small'
+										color='ochre'
+									>
+										Accept
+									</Button>
+									<Button
+										size='small'
+										color='ochre'
+									>
+										Refuse
+									</Button>
+								</ButtonGroup>
+							</ThemeProvider>
+						</Box >
 					</div>
-			</span>
-		)
+				</span>
+			)
 		}
 	}
 }
